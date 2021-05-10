f46a324c8dd46f76bdce0451de00c079ac47fc45
cmo2020 <corio@outlook.com.ar>
2021-05-10 16:46:53 -0300

    Practice.java

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+
+import java.awt.*;
+
+public class Practice {
+
+public static void main (String [ ] args){
+
+    Point punto1 = new Point(1, 1);
+    Point punto2 = punto1;
+    punto1.x = 2;
+    System.out.println(punto2);
+}
+        }
